c3bf8c64a2e921c218722d0c2c27641e603cd0c7
dmangola <sigdananderson@gmail.com>
2021-01-16 13:00:14 +0100

    16Jan_Midday

---
+++ 00-libft/ft_split.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmangola <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/16 11:21:22 by dmangola          #+#    #+#             */
+/*   Updated: 2021/01/16 12:58:23 by dmangola         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	conta_parole(char const *s, char c)
+{
+	int	i;
+	int	parole;
+
+	i = 0;
+	parole = 0;
+	while(s[i])
+	{
+		if(s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
+			parole ++;
+		i++;
+	}
+	return (parole);
+}
+
+static int	lung_parola(char const *s, char c)
+{
+	int i;
+	int lung;
+
+	i = 0;
+	lung = 0;
+	while (s[i] != c && s[i] != '\0')
+	{
+		i++;
+		lung++;
+	}
+	return (lung);
+}
+
+static void	*libera(char **splittato, int parola)
+{
+	int i;
+
+	i = 0;
+	while (i < parola)
+	{
+		free(splittato[i]);
+		i++;
+	}
+	free(splittato);
+	return (NULL);
+}
+
+static char	**riempi(char const *s, int parole, char c, char **splittato)
+{
+	int	i;
+	int	j;
+	int	lung;
+
+	i = -1;
+	while (++i < parole)
+	{
+		while (*s == c)
+			s++;
+		lung = lung_parola(s, c);
+		if (!(splittato[i] = (char *)malloc(sizeof(char) * (lung + 1))))
+			return (libera(splittato, i));
+		j = 0;
+		while (j < lung)
+			splittato [i][j++] = *s++;
+		splittato [i][j] = '\0';
+	}
+	splittato[i] = NULL;
+	return (splittato);
+}
+
+char **ft_split(char const *s, char c)
+{
+	char	**splittato;
+	int		num_parole;
+
+	if (!s)
+		return (NULL);
+	num_parole = conta_parole(s, c);
+	if (!(splittato = (char **) malloc(sizeof(char*) * (num_parole + 1))))
+		return (NULL);
+	splittato = riempi (s, num_parole, c, splittato);
+	return (splittato);
+}
--- 00-libft/libft.h
+++ 00-libft/libft.h
@@ -37,6 +37,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char    **ft_split(char const *s, char c);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcat(char *dst, const char *src, size_t destsize);
