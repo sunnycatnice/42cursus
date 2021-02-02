f9d7b39e03410ed2e4ef478519c260a10f9c780e
Daniele Mangola <dmangola@e4r8p4.42roma.it>
2021-02-02 18:49:11 +0100

    printf_start

--- 03-printf/ft_printf.c
+++ 03-printf/ft_printf.c
@@ -10,7 +10,94 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_printf(const char *str, ...)
+#include "ft_printf.h"
+#include "libft/libft.h"
+
+static void	specs_init(char specs[9],
+			int (*specificatori[8])(va_list *, t_modificatori))
 {
-	
+	specs[0] = 'c';
+	specs[1] = 's';
+	specs[2] = 'd';
+	specs[3] = 'i';
+	specs[4] = 'p';
+	specs[5] = 'u';
+	specs[6] = 'x';
+	specs[7] = 'X';
+	specs[8] = 0;
+	specificatori[0] = &spec_c;
+	specificatori[1] = &spec_s;
+	specificatori[2] = &spec_d;
+	specificatori[3] = &spec_d;
+	specificatori[4] = &spec_p;
+	specificatori[5] = &spec_u;
+	specificatori[6] = &spec_x;
+	specificatori[7] = &spec_x;
+	specificatori[8] = 0;
+
+}
+
+static	int	aggiungi_specif(const char *str, int *cont, va_list args)
+{
+	char			specs[9];
+	int				(*specificatori[9]) (va_list *, t_modificatori);
+	t_modificatori	modificatori;
+	int				contSpecs;
+
+	contSpecs = 0;
+	specs_init(specs, specificatori);
+	controlla_modif(args, str, cont, &modificatori);
+	if (str[*cont] == '%')
+	{
+		(*cont)++;
+		return (spec_r(modificatori));
+	}
+	while(specificatori[contSpecs])
+	{
+		if (str[*cont] == specs[contSpecs])
+		{
+			(*cont)++;
+			if (contSpecs == 7)
+				modificatori.maiuscoli_x = true;
+				return (specificatori[contSpecs] (args, modificatori));
+		}
+		contSpecs++;
+	}
+	return (-1);
+}
+
+static int	ufficio_stampa(const char *str, va_list *arg, int n)
+{
+	int cont;
+	int ret;
+
+	cont = 0;
+	while(str[cont])
+	{
+		if(str[cont] == '%')
+		{
+			cont++;
+			if((ret = aggiungi_specif(str, &cont, arg)) < 0)
+				return (-1);
+			n = n + ret;
+		}
+		else
+		{
+			ft_putchar_fd(str[cont], FD);
+			cont++;
+			n++;
+		}
+	}
+	return (n);
+}
+
+int			ft_printf(const char *str, ...)
+{
+	va_list	arg;
+	int		n;
+
+	va_start(arg, str);
+	n = ufficio_stampa(str, &arg, 0);
+	va_end(arg);
+	return (n);
 }
--- 03-printf/ft_printf.h
+++ 03-printf/ft_printf.h
@@ -13,13 +13,45 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+# define FD 1
+
+# include <unistd.h>
+# include <stdlib.h>
 # include <stdarg.h>
-# include <stdio.h>
+# include <stdbool.h>
 # include "libft/libft.h"
 
 # define BASE_8		"01234567"
 # define BASE_10	"0123456789"
 # define BASE_16	"0123456789abcdef"
 # define BASE_16X	"0123456789ABCDEF"
+
+typedef enum	e_bandiere
+{
+	zero = 0,
+	meno = 1
+}				t_bandiere;
+
+typedef struct	s_modificatori
+{
+	bool	bandiere[2];
+	int		width;
+	bool	width_stato;
+	int		precisione;
+	bool	maiuscoli_x;
+}				t_modificatori;
+
+ typedef struct	s_par
+{
+	char	sflag;
+	char	zflag;
+	char	prec;
+	int		width;
+	int		prec_len;
+	int		printed;
+	char	error;
+	va_list	arg;
+}				t_par;
+
 
 #endif
