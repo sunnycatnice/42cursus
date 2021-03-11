1fa9ec319890b8db97b11d045591540e994ae339
Daniele Mangola <dmangola@e3r10p3.42roma.it>
2021-03-11 14:47:11 +0100

    gitignore .vscode

--- 05-cub3d/cub3d.c
+++ 05-cub3d/cub3d.c
@@ -12,16 +12,26 @@
 
 #include "cub3d.h"
 
-int     main(void)
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
-    void    *mlx;
-    void    *mlx_win;
-    t_data  img;
+	char *dst;
 
-    mlx = mlx_init();
-    img.img = mlx_new_image(mlx, 1920, 1080);
-    mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-    img.addr = mlx_get_data_addr(img.img, &img.bit_per_pixel, &img.line_length,
-                                &img.endian);
-    mlx_loop(mlx);
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+int		main(int ac, char **av)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00AA0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
 }
--- 05-cub3d/cub3d.h
+++ 05-cub3d/cub3d.h
@@ -10,18 +10,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifdef CUB3D_H
+#ifndef CUB3D_H
 # define CUB3D_H
 
-#include <mlx.h>
+# include <mlx.h>
+# include <math.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
 
-typedef struct  s_data {
-    void        *img;
-    char        *addr;
-    int         bits_per_pixel;
-    int         line_length;
-    int         endian;
-}               t_data;
+typedef struct	s_data
+{
+	void		*img;
+	char		*addr;
+	int			bits_per_pixel;
+	int			line_length;
+	int			endian;
+}				t_data;
 
+typedef struct	s_mlx
+{
+	void		*init;
+	void		*win;
+	t_data		data;
+	t_map_input	map_input;
+	
+}				t_mlx;
+
+
+int				main(int ac, char **av);
 
 #endif
