a8c848ebf01235458120ea23b299c6b3f5774b01
Daniele SunnyCat <dmangola@e3r1p5.42roma.it>
2021-03-31 18:33:07 +0200

    05 - new GNL works

--- 05-cub3d/libs/libft/ft_get_next_line.c
+++ 05-cub3d/libs/libft/ft_get_next_line.c
@@ -13,89 +13,20 @@
 #include "libft.h"
 #include <stdio.h>
 
-static char	*ft_remains(char *db)
+int ft_get_next_line(int fd, char **line)
 {
-	char	*remains;
-	int		index_remains;
-	int		index_line;
-
-	index_line = 0;
-	index_remains = 0;
-	if (!db)
-		return (0);
-	while (db[index_line] && db[index_line] != '\n')
-		index_line++;
-	if (!db[index_line])
-	{
-		free(db);
-		return (0);
-	}
-	remains = malloc(sizeof(char) * ((ft_strlen(db) - index_line) + 1));
-	if (!(remains))
-		return (0);
-	index_line++;
-	while (db[index_line])
-		remains[index_remains++] = db[index_line++];
-	remains[index_remains] = '\0';
-	free(db);
-	return (remains);
-}
-
-static char	*ft_copy_in_line(char *db)
-{
-	size_t		index;
-	char		*line;
-
-	index = 0;
-	if (!db)
-		return (0);
-	while (db[index] && db[index] != '\n')
-		index++;
-	line = malloc(sizeof(char) * (index + 1));
-	if (!line)
-		return (0);
-	index = 0;
-	while (db[index] && db[index] != '\n')
-	{
-		line[index] = db[index];
-		index++;
-	}
-	line[index] = '\0';
-	return (line);
-}
-
-static void	ft_doyourjob(char *line, char *db, int fd, char *buffer)
-{
-	*line = *ft_copy_in_line(&db[fd]);
-	db[fd] = *ft_remains(&db[fd]);
-	free(buffer);
-}
-
-int	ft_get_next_line(const int fd, char **line)
-{
-	char		*buffer;
-	static char	*db[MAX_FD];
-	int			res;
-
-	res = 1;
-	if (fd < 0 || !line || BUFFER_SIZE <= 0)
-		return (-1);
-	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!buffer)
-		return (-1);
-	while (!ft_isline(db[fd]) && res != 0)
-	{
-		res = read(fd, buffer, BUFFER_SIZE);
-		if (res == -1)
-		{
-			free(buffer);
-			return (-1);
-		}
-		buffer[res] = '\0';
-		db[fd] = ft_strjoin(db[fd], buffer);
-	}
-	ft_doyourjob(*line, *db, fd, buffer);
-	if (res == 0)
-		return (0);
-	return (1);
+    char    buffer[512];
+    int     reader , i = 0;
+    if (read(fd, &buffer[0], 0) < 0 || (!line))
+        return (-1);
+    while ((reader = read(fd, &buffer[i], 1)) > 0 && buffer[i] != '\n')
+        i++;
+    buffer[i] = '\0';
+    if (reader != -1 && (*line = malloc(i + sizeof(char))) && (i = -1))
+    {
+        while (buffer[++i])
+            (*line)[i] = buffer[i];
+        (*line)[i] = '\0';
+    }
+    return (reader > 0 ? 1 : reader);
 }
