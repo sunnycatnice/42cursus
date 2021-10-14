9e05932cde551338e84c33b60be15ef1a6d16ac6
Daniele SunnyCat <sonosunnybello@gmail.com>
2021-10-14 18:04:48 +0200

    clear.sh

---
+++ utils/clear.sh
@@ -0,0 +1,7 @@
+rm -rf ~/Library/Caches/*
+rm -rf ~/Library/Application Support/Code/User/*
+rm -rf ~/Library/Application Support/Code/CachedExtensionVSIXs/*
+rm -rf ~/Library/Application Support/Code/Cache/*
+rm -rf ~/Users/dmangola/Library/Application Support/Code/CachedData/*
+rm -rf ~/Users/dmangola/Library/Application Support/Slack/Service Worker/CacheStorage/*
+rm -rf ~/Library/Application Support/Google/Chrome/Default/Service Worker/CacheStorage/*
