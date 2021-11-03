ed927a829cda65312ee16eaccfeb8a88f423087e
Daniele Mangola <dmangola@e4r14p4.42roma.it>
2021-11-04 00:38:59 +0100

    andrea sala

--- utils/server_config.sh
+++ utils/server_config.sh
@@ -17,3 +17,11 @@ echo "alias gita='git add *'" >> ~/.zshrc
 echo "alias gitc='git commit -m'" >> ~/.zshrc
 echo "alias gitp='git push'" >> ~/.zshrc
 echo "alias spazio='du -sh * | sort -hr | head -n10'">> ~/.zshrc
+
+if [ -d "~/.oh-my-zsh" ]; then
+	echo "Installing oh my zsh"
+	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	echo "Installed!"
+else
+	echo "Hai oh my zsh installato! Doing nothing..."
+fi
