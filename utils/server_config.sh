1fa00fc80ecdb8fcb75990ebe72cc4e7ca3c4b81
Daniele SunnyCat <sonosunnybello@gmail.com>
2021-11-03 23:00:52 +0100

    alias ok

--- utils/server_config.sh
+++ utils/server_config.sh
@@ -1,5 +1,19 @@
 #to use ssh on a new device or when server is new/reset
 #ssh-keyscan -t ecdsa my.server.domain >> ~/.ssh/known_hosts
 
-yum install git
-yum install vim
+#yum install git
+#yum install vim
+
+echo "alias lista='ls -G'" >> ~/.zshrc
+echo "alias cambia='/usr/bin/cd'" >> ~/.zshrc
+echo "alias cartella='pwd'" >> ~/.zshrc
+echo "alias cerca='/usr/bin/grep'" >> ~/.zshrc
+echo "alias contaparola='/usr/bin/wc'" >> ~/.zshrc
+echo "alias gatto='/usr/bin/git'" >> ~/.zshrc
+echo "alias quale='/usr/bin/which'" >> ~/.zshrc
+echo "alias norma='/usr/bin/norminette'" >> ~/.zshrc
+echo "alias urla='/bin/echo'" >> ~/.zshrc
+echo "alias gita='git add *'" >> ~/.zshrc
+echo "alias gitc='git commit -m'" >> ~/.zshrc
+echo "alias gitp='git push'" >> ~/.zshrc
+echo "alias spazio='du -sh * | sort -hr | head -n10'">> ~/.zshrc
