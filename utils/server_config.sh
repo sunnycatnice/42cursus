e4898d3d51256210062e283c01bd905204369a69
Daniele Mangola <dmangola@e4r13p1.42roma.it>
2021-11-07 01:44:45 +0100

    server config fixed

--- utils/server_config.sh
+++ utils/server_config.sh
@@ -4,20 +4,6 @@
 #yum install git
 #yum install vim
 
-echo "alias lista='ls -G'" >> ~/.zshrc
-echo "alias cambia='/usr/bin/cd'" >> ~/.zshrc
-echo "alias cartella='pwd'" >> ~/.zshrc
-echo "alias cerca='/usr/bin/grep'" >> ~/.zshrc
-echo "alias contaparola='/usr/bin/wc'" >> ~/.zshrc
-echo "alias gatto='/usr/bin/git'" >> ~/.zshrc
-echo "alias quale='/usr/bin/which'" >> ~/.zshrc
-echo "alias norma='/usr/bin/norminette'" >> ~/.zshrc
-echo "alias urla='/bin/echo'" >> ~/.zshrc
-echo "alias gita='git add *'" >> ~/.zshrc
-echo "alias gitc='git commit -m'" >> ~/.zshrc
-echo "alias gitp='git push'" >> ~/.zshrc
-echo "alias spazio='du -sh * | sort -hr | head -n10'">> ~/.zshrc
-
 if [ -d "~/.oh-my-zsh" ]; then
 	echo "Installing oh my zsh"
 	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -25,3 +11,34 @@ if [ -d "~/.oh-my-zsh" ]; then
 else
 	echo "Hai oh my zsh installato! Doing nothing..."
 fi
+
+FILE="/Users/dmangola/.zshrc"
+
+#annamaria aggiunge gli alias alla fine del file .zshrc nella home, se ci sono già, non fa niente
+function annamaria() {
+	if grep -q "$1" $FILE; then
+		echo "$1 found! doing nothing..."
+	else
+		echo $1 >> $FILE
+	fi
+}
+
+annamaria "alias lista='ls -G'"
+annamaria "alias cambia='/usr/bin/cd'"
+annamaria "alias cartella='pwd'"
+annamaria "alias cerca='/usr/bin/grep'"
+annamaria "alias contaparola='/usr/bin/wc'"
+annamaria "alias gatto='/usr/bin/git'"
+annamaria "alias quale='/usr/bin/which'"
+annamaria "alias norma='/usr/bin/norminette'"
+annamaria "alias urla='/bin/echo'"
+annamaria "alias gita='git add *'"
+annamaria "alias gitc='git commit -m'"
+annamaria "alias gitp='git push'"
+annamaria "alias gitl='git pull'"
+annamaria "alias gitd='git diff'"
+annamaria "alias gitb='git branch'"
+annamaria "alias di='say -v luca'"
+annamaria "alias spazio='du -sh * | sort -hr | head -n10'"
+annamaria "alias py3='python3.9'"
+
