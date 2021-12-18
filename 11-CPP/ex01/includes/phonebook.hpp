1fb3426a5fca8f5e0255b740aa02d8817f68a2f3
dani-MacOS <sio2guanoeleo@gmail.com>
2021-12-18 17:54:20 +0100

    ascii art

---
+++ 11-CPP/ex01/includes/phonebook.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmangola <dmangola@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/17 22:14:01 by dmangola          #+#    #+#             */
+/*   Updated: 2021/12/18 17:50:02 by dmangola         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+#define RED "\033[0;31m"
+#define GREEN "\033[0;32m"
+#define RESET "\033[0m"
+
+#include <iostream>
+#include <string>
+
+#endif
