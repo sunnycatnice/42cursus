b49f60aea5a0908635e3ae6803b167ac4abde3ce
dani-MacOS <sio2guanoeleo@gmail.com>
2022-02-09 01:23:04 +0100

    cpp01 ex02 really finished

--- 11-CPP/CPP01/ex02/srcs/main.cpp
+++ 11-CPP/CPP01/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dmangola <dmangola@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 21:34:29 by dmangola          #+#    #+#             */
-/*   Updated: 2022/02/09 01:12:56 by dmangola         ###   ########.fr       */
+/*   Updated: 2022/02/09 01:21:36 by dmangola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,11 @@ int main(void)
 	std::string *strPTR = &str;
 	std::string &strREF = str;
 
-	std::cout << "strPTR: " << strPTR << std::endl;
-	std::cout << "strREF: " << strREF << std::endl;
+	std::cout << "str address: " << &str << std::endl;
+	std::cout << "str using strPTR: " << &(*strPTR) << std::endl;
+	std::cout << "str using strREF: " << &strREF << std::endl << std::endl;
+
+	std::cout << "str: " << str << std::endl;
+	std::cout << "strPTR value: " << strPTR << std::endl;
+	std::cout << "strREF value: " << strREF << std::endl;
 }
